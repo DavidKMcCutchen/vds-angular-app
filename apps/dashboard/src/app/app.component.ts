a9ac1c233b60ec51abff4e62039b7bcd70e2b280
David Kirtland McCutchen <davemccutch@gmail.com>
2025-03-01 18:28:10 -0500

    Added a console log

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
@@ -8,6 +8,12 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
   title = 'dashboard';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('AppComponent initialized');
+  }
 }
